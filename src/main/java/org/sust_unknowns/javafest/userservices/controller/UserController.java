9e85c81ace68f5f48b21781a546c9fd8e36b3886
md-abdullah-92 <abdullahalmahadiapurbo@gmail.com>
2024-09-23 10:26:19 +0600

    Changes to be committed: 	modified:   UserController.java

--- src/main/java/org/sust_unknowns/javafest/userservices/controller/UserController.java
+++ src/main/java/org/sust_unknowns/javafest/userservices/controller/UserController.java
@@ -91,12 +91,6 @@ public class UserController {
     public ResponseEntity<String> loginUser(@RequestBody RequestUser requestUser) {
 
         User user = userService.getUserByEmail(requestUser.getEmail());
-        if (user == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not found");
-        }
-        if (!user.isVerified()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not verified");
-        }
         if (codeGenarator.checkPassword(requestUser.getPassword(), user.getPassword())) {
             String token = jwtUtils.generateToken(user.getId());
             HttpHeaders headers = new HttpHeaders();
